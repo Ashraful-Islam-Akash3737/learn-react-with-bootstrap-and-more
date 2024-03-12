efec94e6589874b4c73752b457d86c3ee10d7559
Ashraful-Islam-Akash <abirashrafulislamabir@gmail.com>
2024-03-12 16:22:41 +0600

    cpmplete this project

--- src/Components/react-bootstrap.jsx
+++ src/Components/react-bootstrap.jsx
@@ -7,7 +7,7 @@ const ReactBootstrap = () => {
   return (
     <Card style={{width:'300px'}}>
       <h3 className='text-blue'>hello from card 3</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic sint eum ab debitis, voluptates ea perspiciatis amet doloremque vitae vel?</p>
+      <p>Lorem ipsum dolor sit amar consectetur adipisicing elit. Hic sint eum ab debitis, voluptates ea perspiciatis amet doloremque vitae vel?</p>
       <Button>Register</Button>
       
     </Card>
